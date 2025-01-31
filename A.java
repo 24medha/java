654b10e5a54a7d4f4c5fdfed50836bfc95e4eec0
24medha <160335573+24medha@users.noreply.github.com>
2025-01-31 18:18:34 +0530

    m

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A {
+    public static void main(String[] args) {
+        int i=10;
+        double m=i;
+        System.out.println(m);
+    }
+    
+
+}
+//Typecasting
---
+++ B.java
@@ -0,0 +1,10 @@
+public class B {
+    public static void main(String[] args) {
+        System.out.println("\033[H\033[2J");
+        int i=2;
+        float m=i;
+        System.out.println(m);
+    }
+    
+}
+//Typecasting
