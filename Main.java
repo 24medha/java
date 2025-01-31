305beb507d2e081789f1cbb3c180e33ba3747f6c
24medha <160335573+24medha@users.noreply.github.com>
2025-01-31 18:28:16 +0530

    medha

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world, Hello Medha");
+    public static void A(int i, int j) {
+        System.out.println(i+j);
     }
+    public static void A(int i, int j , int k) {
+        System.out.println(i+j-k);
+    }
+
+public static void main(String[] args) {
+    System.out.println("\033[H\033[2J");
+    A(2,3,4);
+}
+    
 }
